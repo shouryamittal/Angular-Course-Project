beaebeb97dad3f5c03a8c919407cdbce1f14c7dd
shouryamittal <mittalshourya97@gmail.com>
2018-08-01 00:32:41 +0530

    added minor changes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,7 @@
 import {Recipe} from './recipe.model';
 import { EventEmitter } from 'events';
 export class RecipeService {
-    recipeSelected= new EventEmitter<Recipe>();
+    recipeSelected=new EventEmitter<Recipe>();
    private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
         new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
